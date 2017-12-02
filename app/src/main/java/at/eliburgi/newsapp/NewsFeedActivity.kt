3d77e101b7ba162b79a923bac205abcd84edaf2e
Elias <elias.burgstaller@gmx.at>
2017-12-02 11:20:08 +0100

    Initial commit

---
+++ app/src/main/java/at/eliburgi/newsapp/NewsFeedActivity.kt
@@ -0,0 +1,16 @@
+package at.eliburgi.newsapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class NewsFeedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_feed)
+    }
+
+
+
+
+}
